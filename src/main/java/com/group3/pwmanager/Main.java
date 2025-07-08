b78de21ec6c79dbd3089ba0012703c1d32531e0e
haminpants <samueltfong03@gmail.com>
2025-07-07 18:04:22 -0700

    wip gui

--- src/main/java/com/group3/pwmanager/Main.java
+++ src/main/java/com/group3/pwmanager/Main.java
@@ -1,7 +1,19 @@
 package com.group3.pwmanager;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.group3.pwmanager.forms.HomeMenu;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        FlatDarkLaf.setup();
 
+        JFrame applicationFrame = new JFrame();
+        applicationFrame.setTitle("Password Manager");
+        applicationFrame.setContentPane(new HomeMenu(applicationFrame).getContentPane());
+        applicationFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        applicationFrame.pack();
+        applicationFrame.setVisible(true);
     }
 }
---
+++ src/main/java/com/group3/pwmanager/forms/HomeMenu.java
@@ -0,0 +1,16 @@
+package com.group3.pwmanager.forms;
+
+import javax.swing.*;
+
+public class HomeMenu {
+    private JPanel contentPane;
+    private JLabel l_title;
+    private JButton b_new;
+
+    public HomeMenu(JFrame parent) {
+    }
+
+    public JPanel getContentPane() {
+        return contentPane;
+    }
+}
