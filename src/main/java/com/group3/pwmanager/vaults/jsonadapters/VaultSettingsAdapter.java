0bfc2ff2192d3b1ae6242923168808902419dcea
haminpants <samueltfong03@gmail.com>
2025-07-11 16:24:44 -0700

    implement vault settings deserializer

--- src/main/java/com/group3/pwmanager/vaults/jsonadapters/VaultSettingsAdapter.java
+++ src/main/java/com/group3/pwmanager/vaults/jsonadapters/VaultSettingsAdapter.java
@@ -1,18 +1,23 @@
 package com.group3.pwmanager.vaults.jsonadapters;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+import com.google.gson.*;
 import com.group3.pwmanager.vaults.VaultSettings;
 
 import java.lang.reflect.Type;
 
-public class VaultSettingsAdapter implements JsonSerializer<VaultSettings> {
+public class VaultSettingsAdapter implements JsonSerializer<VaultSettings>, JsonDeserializer<VaultSettings> {
     @Override
     public JsonElement serialize (VaultSettings settings, Type type, JsonSerializationContext jsonSerializationContext) {
         JsonObject jsonSettings = new JsonObject();
         jsonSettings.addProperty("minimizeOnClose", settings.isMinimizeOnClose());
         return jsonSettings;
+    }
+
+    @Override
+    public VaultSettings deserialize (JsonElement jsonElement, Type t, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject json = jsonElement.getAsJsonObject();
+        VaultSettings settings = new VaultSettings();
+        settings.setMinimizeOnClose(json.get("minimizeOnClose").getAsBoolean());
+        return settings;
     }
 }
