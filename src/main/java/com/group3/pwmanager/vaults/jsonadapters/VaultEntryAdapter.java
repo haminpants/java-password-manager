f00f4215515acc5770c9eaf75cb350a82a30fa76
haminpants <samueltfong03@gmail.com>
2025-07-11 16:19:55 -0700

    implement vault entry deserializer

--- src/main/java/com/group3/pwmanager/vaults/jsonadapters/VaultEntryAdapter.java
+++ src/main/java/com/group3/pwmanager/vaults/jsonadapters/VaultEntryAdapter.java
@@ -5,7 +5,7 @@ import com.group3.pwmanager.vaults.VaultEntry;
 
 import java.lang.reflect.Type;
 
-public class VaultEntryAdapter implements JsonSerializer<VaultEntry> {
+public class VaultEntryAdapter implements JsonSerializer<VaultEntry>, JsonDeserializer<VaultEntry> {
     @Override
     public JsonElement serialize (VaultEntry entry, Type t, JsonSerializationContext context) {
         JsonArray jsonTags = new JsonArray();
@@ -15,7 +15,17 @@ public class VaultEntryAdapter implements JsonSerializer<VaultEntry> {
         jsonEntry.addProperty("title", entry.getTitle());
         jsonEntry.addProperty("username", entry.getUsername());
         jsonEntry.addProperty("password", entry.getPassword());
+        jsonEntry.addProperty("note", entry.getNote());
         jsonEntry.add("tags", jsonTags);
         return jsonEntry;
+    }
+
+    @Override
+    public VaultEntry deserialize (JsonElement jsonElement, Type t, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject json = jsonElement.getAsJsonObject();
+        VaultEntry entry = new VaultEntry(json.get("title").getAsString(), json.get("username")
+            .getAsString(), json.get("password").getAsString(), json.get("note").getAsString());
+        json.get("tags").getAsJsonArray().forEach(e -> entry.addTag(e.getAsString()));
+        return entry;
     }
 }
