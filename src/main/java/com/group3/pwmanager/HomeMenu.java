1729f634058573e11f3d0dbb260aa9172c2634db
haminpants <samueltfong03@gmail.com>
2025-07-11 17:09:31 -0700

    implement loading file

--- src/main/java/com/group3/pwmanager/HomeMenu.java
+++ src/main/java/com/group3/pwmanager/HomeMenu.java
@@ -1,21 +1,64 @@
 package com.group3.pwmanager;
 
+import com.google.gson.Gson;
+import com.group3.pwmanager.vaults.Vault;
+import com.group3.pwmanager.vaults.VaultMenu;
+
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileSystemView;
 import java.awt.*;
+import java.io.*;
 
-public class HomeMenu {
-    public static final Dimension PREFERRED_SIZE = new Dimension(300, 400);
-
+public class HomeMenu implements Menu {
     private JPanel contentPane;
     private JButton btn_newVault;
     private JButton btn_loadVault;
     private JList<String> lst_recentVaults;
 
-    public HomeMenu() {
+    private final JFileChooser fileChooser = new JFileChooser();
 
+    public HomeMenu () {
+        fileChooser.setFileFilter(new FileFilter() {
+            @Override
+            public String getDescription () {
+                return "Java Password Manager Vault (*.jpmv)";
+            }
+
+            @Override
+            public boolean accept (File file) {
+                return file.getName().toLowerCase().endsWith(".jpmv");
+            }
+        });
+
+        btn_loadVault.addActionListener(event -> {
+            if (fileChooser.showOpenDialog(contentPane) != JFileChooser.APPROVE_OPTION) return;
+            try (FileReader fileReader = new FileReader(fileChooser.getSelectedFile());
+                BufferedReader reader = new BufferedReader(fileReader)) {
+                StringBuilder contents = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    contents.append(line);
+                }
+                Vault vault = Main.GSON.fromJson(contents.toString(), Vault.class);
+                Main.setAppContent(new VaultMenu(vault));
+            }
+            catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
-    public JPanel getContentPane() {
+    @Override
+    public JPanel getContentPane () {
         return contentPane;
+    }
+
+    @Override
+    public Dimension getPreferredSize () {
+        return new Dimension(300, 400);
     }
 }
--- src/main/java/com/group3/pwmanager/Main.java
+++ src/main/java/com/group3/pwmanager/Main.java
@@ -22,18 +22,7 @@ public class Main {
 
     public static void main (String[] args) {
         FlatDarkLaf.setup();
-
-        Vault vault = new Vault("Test Vault");
-        vault.getSettings().setMinimizeOnClose(true);
-        vault.addEntry(new VaultEntry("Test 1", "test@test.com", "wow", "hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world hello world "));
-        vault.addEntry(new VaultEntry("Test 2", "test@test.com", "cool!", "hello world"));
-        vault.addEntry(new VaultEntry("Test 3", "test@test.com", "cooler", "hello world"));
-        vault.addEntry(new VaultEntry("Test 4", "test@test.com", "damn", "hello world"));
-        vault.addEntry(new VaultEntry("Test 5", "test@test.com", "pls work", "hello world"));
-        vault.addEntry(new VaultEntry("Test 6", "test@test.com", "it work!!!1", "hello world"));
-        vault.addEntry(new VaultEntry("Test 7", "test@test.com", "yay :)", "hello world"));
-        VaultMenu menu = new VaultMenu(vault);
-        setAppContent(menu);
+        setAppContent(new HomeMenu());
 
         appFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         appFrame.setLocationRelativeTo(null);
