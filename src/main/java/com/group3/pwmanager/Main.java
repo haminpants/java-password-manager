5f00db0a3d4c2bc0545cbc545e2fee4b4e5cc82b
haminpants <samueltfong03@gmail.com>
2025-07-13 03:08:34 -0700

    Update Main.java

--- src/main/java/com/group3/pwmanager/Main.java
+++ src/main/java/com/group3/pwmanager/Main.java
@@ -21,9 +21,11 @@ public class Main {
 
     public static void main (String[] args) {
         FlatDarkLaf.setup();
-        setAppContent(new HomeMenu());
 
+        HomeMenu homeMenu = new HomeMenu();
         appFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        appFrame.setContentPane(homeMenu.getContentPane());
+        appFrame.pack();
         appFrame.setLocationRelativeTo(null);
         appFrame.setVisible(true);
     }
@@ -33,5 +35,6 @@ public class Main {
         appFrame.setJMenuBar(menu.getMenuBar());
         appFrame.pack();
         appFrame.revalidate();
+        appFrame.setLocationRelativeTo(null);
     }
 }
