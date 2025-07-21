56a5faccd66644425d0c34ccfefc916c463f6ab9
haminpants <samueltfong03@gmail.com>
2025-07-20 23:50:13 -0700

    Update Main.java

--- src/main/java/com/group3/pwmanager/Main.java
+++ src/main/java/com/group3/pwmanager/Main.java
@@ -2,10 +2,23 @@ package com.group3.pwmanager;
 
 import com.formdev.flatlaf.FlatDarkLaf;
 
+import javax.swing.*;
+import java.io.PrintWriter;
+
 public class Main {
     public static void main (String[] args) {
-        FlatDarkLaf.setup();
-        HomeMenu menu = new HomeMenu();
-        menu.setVisible(true);
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+            SwingUtilities.invokeLater(() -> new HomeMenu().setVisible(true));
+        }
+        catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+            try (PrintWriter out = new PrintWriter("error.log")) {
+                e.printStackTrace(out);
+            }
+            catch (Exception ioEx) {
+                ioEx.printStackTrace();
+            }
+        }
     }
 }
